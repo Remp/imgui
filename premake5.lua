4ea869b63cdccaa9721cc6e3ed56e450c4b2b768
Kirill Kogut <kirilrempov@gmail.com>
2021-11-19 19:37:54 +0200

    Added premake support for macos only

---
+++ premake5.lua
@@ -0,0 +1,33 @@
+project "ImGui"
+	kind "StaticLib"
+	language "C++"
+	staticruntime "off"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+        "imconfig.h",
+        "imgui_draw.cpp",
+        "imgui_internal.h",
+        "imgui_tables.cpp",
+        "imgui_widgets.cpp",
+        "imgui.cpp",
+        "imgui.h",
+        "imstb_rectpack.h",
+        "imstb_textedit.h",
+        "imstb_truetype.h",
+		"imgui_demo.cpp"
+	}
+
+	filter "system:macosx"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
